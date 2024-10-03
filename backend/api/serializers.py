2463d370d8ac5b49315c23afb05f885283fb6e63
artem <artyom-ivanov-spb@yandex.ru>
2024-10-03 11:41:41 +0200

    Fixes after 1 review. Remove unused code.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -24,32 +24,6 @@ class UserCreateSerializer(UserCreateSerializer):
                   'password')
 
 
-class CustomUserCreateSerializer(serializers.ModelSerializer):
-    """Собственный сериализатор создания юзера при регистрации."""
-
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'username',
-            'email',
-            'first_name',
-            'last_name',
-            'password'
-        )
-
-    def validate_username(self, value):
-        if User.objects.filter(username=value).exists():
-            raise serializers.ValidationError(
-                f'Юзернейм {value} уже занят.')
-        return value
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError(f'{value} уже зарегистрирован.')
-        return value
-
-
 class UserSerializer(serializers.ModelSerializer):
     """Сериализатор для получения, обновления информации о пользователе"""
 
